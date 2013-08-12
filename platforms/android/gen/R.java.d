c5a52bfdb6303f8fe16a3893a3fb1a6c6bbe2f69
Richard Lee <lifuzu@gmail.com>
2013-08-11 23:30:36 -0700

    initial commit

---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,14 @@
+/Users/rlee/workspace/skyfot/platforms/android/gen/com/lifuzu/skyfot/R.java \
+ : /Users/rlee/workspace/skyfot/platforms/android/res/drawable/icon.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-hdpi/ic_launcher.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-hdpi/icon.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-ldpi/ic_launcher.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-ldpi/icon.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-mdpi/ic_launcher.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-mdpi/icon.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-xhdpi/ic_launcher.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/drawable-xhdpi/icon.png \
+/Users/rlee/workspace/skyfot/platforms/android/res/layout/main.xml \
+/Users/rlee/workspace/skyfot/platforms/android/res/values/strings.xml \
+/Users/rlee/workspace/skyfot/platforms/android/res/xml/config.xml \
+/Users/rlee/workspace/skyfot/platforms/android/bin/AndroidManifest.xml \
---
+++ server/app.js
@@ -0,0 +1,90 @@
+var express = require('express');
+
+var app = express()
+
+var fs = require('fs');
+var fsextra = require('fs.extra');
+var im = require('imagemagick');
+
+var Db = require('mongodb').Db;
+var dbServer = require('mongodb').Server;
+var dbConnection = require('mongodb').Connection;
+
+var db = new Db('photos', new dbServer('localhost', dbConnection.DEFAULT_PORT, {}));
+db.open(function(err, db){});
+
+app.use(express.bodyParser())
+
+app.get('/', function(req, res){
+    res.send(
+        '<form action="/upload" method="post" enctype="multipart/form-data">'+
+        '<input type="file" name="source">'+
+        '<input type="submit" value="Upload">'+
+        '</form>'
+    );
+});
+
+app.post('/upload', function(req, res){
+	console.log("Received file:\n" + JSON.stringify(req.files));
+
+	var photoDir = __dirname+"/photos/";
+	var thumbnailsDir = __dirname+"/photos/thumbnails/";
+	var photoName = req.files.source.name;
+
+	console.log(photoDir+photoName);
+
+	fsextra.move(
+		req.files.source.path,
+		photoDir+photoName,
+		function(err){
+			if(err != null){
+				console.log(err)
+				res.send({error:"Server Writting No Good"});
+			} /*else {
+				im.resize(
+					{
+						srcData:fs.readFileSync(photoDir+photoName, 'binary'),
+						width:256
+					}, 
+					function(err, stdout, stderr){
+						if(err != null){
+							console.log('stdout : '+stdout)
+
+							res.send({error:"Resizeing No Good"});
+						} else {
+							//console.log('ELSE stdout : '+stdout)
+							fs.writeFileSync(thumbnailsDir+"thumb_"+photoName, stdout, 'binary');
+							res.send("Ok");
+						}
+					}
+				);
+			}*/
+		}
+	);
+});
+
+app.get('/info', function(req, res){
+	console.log(__dirname);
+	res.send("ok");
+});
+
+app.listen(8000);
+console.log('connected to localhost....')
+
+/*
+var fs = require('fs');
+//var util = require('util');
+
+var is = fs.createReadStream('source_file');
+var os = fs.createWriteStream('destination_file');
+
+is.pipe(os);
+is.on('end',function() {
+    fs.unlinkSync('source_file');
+});*/
+
+/* node.js 0.6 and earlier you can use util.pump:
+util.pump(is, os, function() {
+    fs.unlinkSync('source_file');
+});
+*/
